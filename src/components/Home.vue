9a9464e48b1f31e96394589f73c18366e2354664
mahiyou <you.maede@gmail.com>
2023-10-22 20:58:58 +0330

    Home became responsive.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,20 @@
 <template>
     <v-container class="home-container">
         <v-row class="row-height">
-            <v-col cols="5" class="ma-auto"><v-img src="../assets/pics/me.jpg" class="img mr-5"></v-img></v-col>
-            <v-col cols="7" class="ma-auto">
+            <v-col cols="12" md="5" class="ma-auto"><v-img src="../assets/pics/me.jpg"
+                    class="img mr-md-5 mx-auto"></v-img></v-col>
+            <v-col cols="12" md="7" class="ma-auto text-md-left text-center">
                 <p class="name">Maedeh Yousefi</p>
-                <p>I am a Front-End develoaper.</p>
-                <p>I create beautiful and usefule products and try to help users by building products with a delightful user experience.</p>
+                <div class="my-md-1 my-5">
+                    <p>I am a Front-End develoaper.</p>
+                    <p>I create beautiful and usefule products and try to help users by building products with a delightful
+                        user
+                        experience.</p>
+                </div>
                 <v-btn variant="text" href="https://github.com/mahiyou" icon="mdi-github"></v-btn>
                 <v-btn variant="text" icon="mdi-linkedin"></v-btn>
-                <v-btn variant="text" icon="mdi-gmail"></v-btn>  
-                <v-btn variant="text" icon="mdi-instagram"></v-btn>  
+                <v-btn variant="text" icon="mdi-gmail"></v-btn>
+                <v-btn variant="text" icon="mdi-instagram"></v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -21,9 +26,10 @@
 
 <style lang="scss">
 .home-container {
-    .row-height{
+    .row-height {
         height: 100%;
     }
+
     .img {
         border-radius: 50%;
         width: 250px;
@@ -35,6 +41,12 @@
     .name {
         font-weight: 900;
         font-size: 60px;
+    }
+
+    @media(max-width: 700px) {
+        .name {
+            font-size: 40px;
+        }
     }
 }
 </style>
--- src/layouts/default/Navigation.vue
+++ src/layouts/default/Navigation.vue
@@ -6,16 +6,43 @@
                 <p class="mb-8">Front-End Developer</p>
             </v-list-item>
             <v-list-item link title="Home" :to="{ name: 'home' }" active-color="white"></v-list-item>
-            <v-list-item link title="About" :to="{ name: 'about' }"  active-color="white"></v-list-item>
-            <v-list-item link title="Portfolio" :to="{ name: 'portfolio' }"  active-color="white"></v-list-item>
+            <v-list-item link title="About" :to="{ name: 'about' }" active-color="white"></v-list-item>
+            <v-list-item link title="Portfolio" :to="{ name: 'portfolio' }" active-color="white"></v-list-item>
             <v-list-item link title="Contact" :to="{ name: 'contact' }"></v-list-item>
         </div>
     </v-navigation-drawer>
+
+    <v-navigation-drawer v-model="drawer" class="nav hidden-lg-and-up" color="#494b50" width="300" justify="center"
+        temporary>
+        <div class="menu-items" justify="center">
+            <v-list-item>
+                <p class="menu-title">Maedeh Yousefi</p>
+                <p class="mb-8">Front-End Developer</p>
+            </v-list-item>
+            <v-list-item link title="Home" :to="{ name: 'home' }" active-color="white"></v-list-item>
+            <v-list-item link title="About" :to="{ name: 'about' }" active-color="white"></v-list-item>
+            <v-list-item link title="Portfolio" :to="{ name: 'portfolio' }" active-color="white"></v-list-item>
+            <v-list-item link title="Contact" :to="{ name: 'contact' }"></v-list-item>
+        </div>
+    </v-navigation-drawer>
+    <v-app-bar color="#494b50" :elevation="0" class="hidden-lg-and-up">
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    data() {
+        return {
+            drawer: false,
+        }
+    }
+})
+</script>
 <style lang="scss">
 .nav {
     --v-theme-overlay-multiplier: 0;
-    
+
     .v-list-item {
         color: #a0a0a2;
         margin-top: 10px;
@@ -32,5 +59,9 @@
         width: 80%;
         margin-top: 40%;
     }
+}
+
+.v-app-bar {
+    box-shadow: none;
 }
 </style>
