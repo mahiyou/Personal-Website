27484de41f4f76a6acd4124fabc5b334cf658995
mahiyou <you.maede@gmail.com>
2023-10-31 19:11:51 +0330

    Some improvments in about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,203 +4,210 @@
         <v-img src="../assets/pics/me2.jpg" class="img"></v-img>
         <p class="title">Maedeh Yousefi</p>
         <p class="sub-title">Front-End Developer</p>
-        <v-divider :thickness="1.5" class="my-5"></v-divider>
-        <div class="content">
-            Hi, welcome to my personal website.<br />
-            I graduated in software engineering with a bachelor’s degree in 2020.<br />
-            After graduation I experienced different aspects of IT industry and finally decided to work as a front-end developer.<br />
-            My priority in every project is customer convenience in using the user interface, and after that beauty and
-            creativity is important.
+        <v-divider :thickness="2" class="my-5"></v-divider>
+        <div>
+            Front-end developer with 2+ years of work experience.
+            Bachelor degree in software engineering with an android application as final project.
+            Has several successfuly closed front-end projects using vue.
+            js and react along with the programming team of Dade Negar Jey Ltd.
+            Ability to convert any complex graphic design to code with high attention to details.
+            Eager to learn new skills through Searching and use them in projects.
+
         </div>
-        <v-divider :thickness="1.5" class="my-10"></v-divider>
+        <v-divider :thickness="2" class="my-10"></v-divider>
         <v-row>
-            <v-col>
+            <v-col cols="6">
                 <v-table>
                     <tbody>
                         <tr>
-                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-2">mdi-cake-variant</v-icon>Birthday:</td>
-                            <td class="pr-0 text-white font-weight-bold">1998</td>
+                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-1">mdi-email-outline</v-icon>Email:
+                            </td>
+                            <td class="pr-0 font-weight-bold"><a href="/contact"
+                                    class="text-white text-decoration-none">me@mahiyou.net</a></td>
                         </tr>
                         <tr>
-                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-2">mdi-email-outline</v-icon>Email:</td>
-                            <td class="pr-0 text-white font-weight-bold"><a href="/contact" class="text-white text-decoration-none">you.maede@gmail.com</a></td>
+                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-1">mdi-github</v-icon>Github:</td>
+                            <td class="pr-0 font-weight-bold"><a href="https://github.com/mahiyou"
+                                    class="text-white text-decoration-none">github.com/mahiyou</a></td>
                         </tr>
                         <tr>
-                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-2">mdi-github</v-icon>github:</td>
-                            <td class="pr-0 text-white font-weight-bold"><a href="https://github.com/mahiyou" class="text-white text-decoration-none">github.com/mahiyou</a></td>
-                        </tr>
-                    </tbody>
-                </v-table>
-            </v-col>
-            <v-col>
-                <v-table>
-                    <tbody>
-                        <tr>
-                            <td class="font-weight-bol pl-0"><v-icon class="mr-2 mb-2"> mdi-book-open-page-variant-outline</v-icon>Major:</td>
-                            <td class="pr-0 text-white font-weight-bold">Software Engineering</td>
-                        </tr>
-                        <tr>
-                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-2">mdi-account-school</v-icon>Deegree:</td>
-                            <td class="pr-0 text-white font-weight-bold">Bachelor</td>
-                        </tr>
-                        <tr>
-                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-2">mdi-laptop</v-icon>Freelance:</td>
-                            <td class="pr-0 text-white font-weight-bold">Available</td>
+                            <td class="font-weight-bold pl-0"><v-icon class="mr-2 mb-1">mdi-linkedin</v-icon>LinkedIn:</td>
+                            <td class="pr-0 font-weight-bold"><a href="linkedin.com/in/mahiyou"
+                                    class="text-white text-decoration-none">linkedin.com/in/mahiyou</a></td>
                         </tr>
                     </tbody>
                 </v-table>
             </v-col>
         </v-row>
         <v-btn variant="outlined" class="mt-10">Download CV</v-btn>
-        <v-divider :thickness="1.5" class="my-10"></v-divider>
-        <div class="title text-white">Skills</div>
+        <v-divider :thickness="2" class="my-10"></v-divider>
         <v-row>
-            <v-col cols="12" md="6" class="pr-md-10">
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">Html & Css</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">100%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="100" color="#00FFFF" :height="3"></v-progress-linear>
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">Vue</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">70%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="70" color="#7cfefe" :height="3"></v-progress-linear>
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">React</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">70%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="70" color="#7cfefe" :height="3"></v-progress-linear>
-            </v-col>
-            <v-col cols="12" md="6" class="pl-md-10">
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">JavaScript</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">60%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="60" :height="3" color="#bdffff"></v-progress-linear>
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">Java</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">70%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="70" color="#7cfefe" :height="3"></v-progress-linear>
-            </v-col>
-        </v-row>
-        <div class="title text-white mt-15">Languages</div>
-        <v-row>
-            <v-col cols="12" md="6" class="pr-md-10">
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">English</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">80%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="80" color="#5bffff" :height="3"></v-progress-linear>
-            </v-col>
-            <v-col cols="12" md="6" class="pl-md-10">
-                <v-row class="mt-5">
-                    <v-col>
-                        <div class="text-white">Farsi</div>
-                    </v-col>
-                    <v-col>
-                        <div class="text-right">100%</div>
-                    </v-col>
-                </v-row>
-                <v-progress-linear model-value="100" :height="3" color="#00FFFF"></v-progress-linear>
-            </v-col>
-        </v-row>
-        <v-divider :thickness="1.5" class="my-15"></v-divider>
-        <v-row>
-            <v-col cols="12" sm="9" md="6" class="mb-10 pr-md-6 pl-6 pl-md-auto">
-                <div class="title text-white mb-10 mt-0">Work Experience</div>
+            <v-col cols="12" class="mb-10 pr-md-6 pl-6 pl-md-auto">
+                <div class="title mb-10 mt-0">Work Experience</div>
                 <v-row class="left-border">
                     <v-col cols="1" class="mr-0 my-auto">
                         <div class="circle"></div>
                     </v-col>
-                    <v-col cols="4" class="my-auto pl-0" align="center">
-                        <div class="background text-white">2022 - now</div>
+                    <v-col cols="2" class="my-auto pl-0" align="center">
+                        <div class="background">2022 - now</div>
                     </v-col>
-                    <v-col cols="7" class="my-auto">
-                        <div class="text-white font-weight-bold">Dade Negar Jey Company</div> 
-                        <div>Front-End Developer</div>
-                        <div>(JavaScript, React, Vue, ...)</div>
-                        <v-btn variant="outlined" height="24px" width="100px" class="btn-site" href="https://www.jeyserver.com/">website</v-btn>
+                    <v-col cols="4" class="my-auto" align="center">
+                        <div class="font-weight-bold">Front-End Developer</div>
+                    </v-col>
+                    <v-col cols="5" class="my-auto">
+                        <div>Dade Negar Jey Company</div>
                     </v-col>
                 </v-row>
                 <v-row class="left-border">
+                    <v-col cols="11" offset="1">
+                        <ul>
+                            <li>Converted graphic designs to code and developing front end of new sites and new projects.
+                            </li>
+                            <li>Fixed front-end buges and errors in old projects.</li>
+                            <li>Learned several useful frameworks and libraries of front-end</li>
+                        </ul>
+                        <v-btn variant="outlined" height="27px" width="120px" class="btn-site mt-6"
+                            href="https://www.jeyserver.com/">website</v-btn>
+                    </v-col>
+                </v-row>
+                <v-row class="left-border pt-8">
                     <v-col cols="1" class="mr-0 my-auto">
                         <div class="circle"></div>
                     </v-col>
-                    <v-col cols="4" class="my-auto pl-0" align="center">
-                        <div class="background text-white">2018 - 2019</div>
+                    <v-col cols="2" class="my-auto pl-0" align="center">
+                        <div class="background">2018 - 2019</div>
                     </v-col>
-                    <v-col cols="7" class="my-auto">
-                        <div class="text-white font-weight-bold">Hub company</div>
-                        <div>Android Developer</div>
-                        <div>(Java)</div>
-                        <v-btn variant="outlined" height="24px"  width="100px" class="btn-site" href="https://isfahanhub.com/">website</v-btn>
+                    <v-col cols="4" class="my-auto" align="center">
+                        <div class="font-weight-bold">Android Developer</div>
+                    </v-col>
+                    <v-col cols="5" class="my-auto">
+                        <div>Hub company</div>
+                    </v-col>
+                </v-row>
+                <v-row class="left-border">
+                    <v-col cols="11" offset="1">
+                        <ul>
+                            <li>Designed android aplications with java.</li>
+                        </ul>
+                        <v-btn variant="outlined" height="27px" width="120px" class="btn-site mt-6"
+                            href="https://isfahanhub.com/">website</v-btn>
                     </v-col>
                 </v-row>
             </v-col>
-            <v-col cols="12" sm="9" md="6" class="pl-6">
-                <div class="title text-white mb-10 mt-0">Education</div>
+            <v-divider :thickness="2" class="mt-15 mb-10"></v-divider>
+            <v-col cols="12" class="pl-6">
+                <div class="title mb-10 mt-0">Education</div>
                 <v-row class="left-border">
                     <v-col cols="1" class="mr-0 my-auto">
                         <div class="circle"></div>
                     </v-col>
-                    <v-col cols="4" class="my-auto pl-0" align="center">
-                        <div class="background text-white">2016 - 2020</div>
+                    <v-col cols="2" class="my-auto pl-0" align="center">
+                        <div class="background">2016 - 2020</div>
                     </v-col>
-                    <v-col cols="7" class="my-auto">
-                        <div class="text-white font-weight-bold">Azad University Of Najafabad</div>
-                        <div>Bachelor degree In Software Engineering </div>
-                        <v-btn variant="outlined" height="24px" width="100px" class="btn-site">certificate</v-btn>
+                    <v-col cols="4" class="my-auto">
+                        <div class="font-weight-bold">Bachelor degree In Software Engineering </div>
+                    </v-col>
+                    <v-col cols="5">
+                        <div>Azad University</div>
                     </v-col>
                 </v-row>
                 <v-row class="left-border">
+                    <v-col cols="11" offset="1">
+                        <ul>
+                            <li>Designed an android aplication for driving theory training as final project</li>
+                        </ul>
+                        <v-btn variant="outlined" height="27px" width="120px" class="btn-site mt-6">certificate</v-btn>
+                    </v-col>
+                </v-row>
+                <v-row class="left-border pt-8">
                     <v-col cols="1" class="mr-0 my-auto">
                         <div class="circle"></div>
                     </v-col>
-                    <v-col cols="4" class="my-auto pl-0" align="center">
-                        <div class="background text-white">2012 - 2016</div>
+                    <v-col cols="2" class="my-auto pl-0" align="center">
+                        <div class="background">2012 - 2016</div>
                     </v-col>
-                    <v-col cols="7" class="my-auto">
-                        <div class="text-white font-weight-bold">Farzanagan High School</div>
-                        <div class="text-white">(School for Brilliant Talents)</div>
-                        <div>Mathematics Diploma</div>
-                        <v-btn variant="outlined" height="24px" width="100px" class="btn-site">certificate</v-btn>
+                    <v-col cols="4" class="my-auto">
+                        <div class="font-weight-bold">Mathematics and Physics Diploma</div>
+                    </v-col>
+                    <v-col cols="5">
+                        <div>Farzanagan High School</div>
+                        <div>(School for Brilliant Talents)</div>
+                    </v-col>
+                </v-row>
+                <v-row class="left-border">
+                    <v-col cols="11" offset="1">
+                        <v-btn variant="outlined" height="27px" width="120px" class="btn-site">certificate</v-btn>
                     </v-col>
                 </v-row>
             </v-col>
         </v-row>
+        <v-divider :thickness="2" class="mt-15 mb-10"></v-divider>
+        <div class="title">Skills</div>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">Vue.js</h3>
+            <span class="skill-background">vuetify</span>
+            <span class="skill-background">vue-router</span>
+            <span class="skill-background">pinia</span>
+            <span class="skill-background">typescript</span>
+            <span class="skill-background">sass</span>
+            <span class="skill-background">less</span>
+            <span class="skill-background">responsive design</span>
+        </div>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">React</h3>
+            <span class="skill-background">react bootstrap</span>
+            <span class="skill-background">fontawesome</span>
+            <span class="skill-background">react-router-dom</span>
+            <span class="skill-background">typescript</span>
+            <span class="skill-background">sass</span>
+            <span class="skill-background">less</span>
+            <span class="skill-background">responsive design</span>
+        </div>
+        <h3 class="font-weight-bold my-2">Html and Css</h3>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">Javascript</h3>
+            <span class="skill-background">promises</span>
+            <span class="skill-background">callback functions</span>
+            <span class="skill-background">asynchronous</span>
+            <span class="skill-background">polymorphism</span>
+            <span class="skill-background">sass</span>
+            <span class="skill-background">less</span>
+            <span class="skill-background">responsive design</span>
+        </div>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">Java</h3>
+            <span class="skill-background">object-Oriented programming</span>
+            <span class="skill-background">polymorphism</span>
+            <span class="skill-background">inheritance</span>
+            <span class="skill-background">interfaces</span>
+            <span class="skill-background">android studio</span>
+        </div>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">other skils</h3>
+            <span class="skill-background">git</span>
+            <span class="skill-background">npm</span>
+        </div>
+        <v-divider :thickness="2" class="mt-15 mb-10"></v-divider>
+        <div class="title mt-15">Languages</div>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">Farsi</h3>
+            <span class="skill-background">Native</span>
+        </div>
+        <div class="my-10">
+            <h3 class="font-weight-bold my-2">English</h3>
+            <span class="skill-background">Speacking: 7 of 9</span>
+            <span class="skill-background">Reading: 6 of 9</span>
+            <span class="skill-background">Listening: 6 of 9</span>
+            <span class="skill-background">Writing: 5.5 of 9</span>
+        </div>
+
     </v-container>
 </template>
 <style lang="scss">
 .v-container.about-container {
     padding: 20px 50px;
     line-height: 30px;
-    color: #b4b4b5;
+    font-size: 16px;
 
     .title {
         font-size: 26px;
@@ -208,9 +215,6 @@
         color: white;
     }
 
-    .content {
-        font-size: 14px;
-    }
 
     .v-table {
         background-color: transparent;
@@ -240,12 +244,21 @@
         padding: 5px;
         font-size: 14px;
     }
-    .btn-site{
+
+    .btn-site {
         font-size: 10px;
+        color: #b4b4b5;
     }
+
+    .skill-background {
+        border: 1px solid #7cfefe;
+        border-radius: 5px;
+        margin: 3px;
+        padding: 7px;
+    }
+
     @media(max-width: 700px) {
         padding: 20px 20px;
-        color: #7cfefe;
     }
 }
 </style>
